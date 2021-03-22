58d561b3a791b5e8c572bf0ddbe981b561b78ee8
Bhagwat Singh Chouhan <bhagwat.chouhan@vulpinecode.com>
2021-03-22 15:56:51 +0530

    Applied quick fixes.

--- admin/views/sidebar.php
+++ admin/views/sidebar.php
@@ -7,6 +7,8 @@ use cmsgears\newsletter\common\config\NewsletterGlobal;
 
 $core	= Yii::$app->core;
 $user	= Yii::$app->core->getUser();
+
+$siteRootUrl = Yii::$app->core->getSiteRootUrl();
 ?>
 <?php if( $core->hasModule( 'newsletter' ) && $user->isPermitted( NewsletterGlobal::PERM_NEWSLETTER_ADMIN ) ) { ?>
 	<div id="sidebar-newsletter" class="collapsible-tab has-children <?= $parent == 'sidebar-newsletter' ? 'active' : null ?>">
@@ -16,12 +18,12 @@ $user	= Yii::$app->core->getUser();
 		</div>
 		<div class="tab-content clear <?= $parent == 'sidebar-newsletter' ? 'expanded visible' : null ?>">
 			<ul>
-				<li class="newsletter <?= $child == 'newsletter' ? 'active' : null ?>"><?= Html::a( 'Newsletters', [ '/newsletter/newsletter/all' ] ) ?></li>
-				<li class="member <?= $child == 'member' ? 'active' : null ?>"><?= Html::a( 'Members', [ '/newsletter/member/all' ] ) ?></li>
-				<li class="list <?= $child == 'list' ? 'active' : null ?>"><?= Html::a( 'Mailing Lists', [ '/newsletter/list/all' ] ) ?></li>
-				<li class="trigger <?= $child == 'trigger' ? 'active' : null ?>"><?= Html::a( 'Newsletter Triggers', [ '/newsletter/trigger/all' ] ) ?></li>
-				<li class="newsletter-template <?= $child == 'newsletter-template' ? 'active' : null ?>"><?= Html::a( 'Newsletter Templates', [ '/newsletter/newsletter/template/all' ] ) ?></li>
-				<li class="edition-template <?= $child == 'edition-template' ? 'active' : null ?>"><?= Html::a( 'Edition Templates', [ '/newsletter/edition/template/all' ] ) ?></li>
+				<li class="newsletter <?= $child == 'newsletter' ? 'active' : null ?>"><?= Html::a( 'Newsletters', [ "$siteRootUrl/newsletter/newsletter/all" ] ) ?></li>
+				<li class="member <?= $child == 'member' ? 'active' : null ?>"><?= Html::a( 'Members', [ "$siteRootUrl/newsletter/member/all" ] ) ?></li>
+				<li class="list <?= $child == 'list' ? 'active' : null ?>"><?= Html::a( 'Mailing Lists', [ "$siteRootUrl/newsletter/list/all" ] ) ?></li>
+				<li class="trigger <?= $child == 'trigger' ? 'active' : null ?>"><?= Html::a( 'Newsletter Triggers', [ "$siteRootUrl/newsletter/trigger/all" ] ) ?></li>
+				<li class="newsletter-template <?= $child == 'newsletter-template' ? 'active' : null ?>"><?= Html::a( 'Newsletter Templates', [ "$siteRootUrl/newsletter/newsletter/template/all" ] ) ?></li>
+				<li class="edition-template <?= $child == 'edition-template' ? 'active' : null ?>"><?= Html::a( 'Edition Templates', [ "$siteRootUrl/newsletter/edition/template/all" ] ) ?></li>
 			</ul>
 		</div>
 	</div>
